21a826b19860a7cacf6187c079d02a81fcefe7fe
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-01 11:49:32 -0300

    FEAT: adds database connection settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
 
+	user := os.Getenv("HABIT_MANAGER_USER")
+	password := os.Getenv("HABIT_MANAGER_PASSWORD")
+	host := "localhost"
+	port := os.Getenv("HABIT_MANAGER_PORT")
+	dbName := os.Getenv("HABIT_MANAGER_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		user, password, host, port, dbName)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("error connecting to MySQL: %v", err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("database did not responsd: %v", err)
+	}
+
+	log.Printl("connected to MySQL!")
 }
