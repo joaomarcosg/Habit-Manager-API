c5b3301875b5d964a7e4702dda2466f618d6a5d1
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-01 12:57:10 -0300

    FEAT: adds user querie

---
+++ internal/store/queries/user.sql
@@ -0,0 +1,14 @@
+-- name: CreateUser :execresult
+INSERT INTO user (
+    id,
+    name,
+    email,
+    password
+)
+VALUES (?,?,?,?);
+
+--name: GetUserById :one
+SELECT * FROM user WHERE id = ?;
+
+--name: GetUserByEmail :one
+SELECT * FROM user WHERE email = ?;
