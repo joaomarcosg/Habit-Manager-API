9679f24a8157c122aee9d1c854e13cafaab31a1e
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-06 18:41:31 -0300

    FEAT: adds MySQL implementation

--- internal/store/mysqlstore/ms_user_store.go
+++ internal/store/mysqlstore/ms_user_store.go
@@ -1,1 +1,19 @@
 package mysqlstore
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MSUserStore struct {
+	Queries *Queries
+	Pool    *sql.DB
+}
+
+func NewMSUserStore(pool *sql.DB) MSUserStore {
+	return MSUserStore{
+		Queries: New(pool),
+		Pool:    pool,
+	}
+}
