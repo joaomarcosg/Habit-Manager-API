b4d25d73bdbb96a6f1c6e126005c9ad0285b0928
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-22 12:47:53 -0300

    FEAT: implements CreateUser method of UserStore interface

--- internal/store/mysqlstore/ms_user_store.go
+++ internal/store/mysqlstore/ms_user_store.go
@@ -1,9 +1,11 @@
 package mysqlstore
 
 import (
+	"context"
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 )
 
 type MSUserStore struct {
@@ -16,4 +18,27 @@ func NewMSUserStore(pool *sql.DB) MSUserStore {
 		Queries: New(pool),
 		Pool:    pool,
 	}
+}
+
+func (msu *MSUserStore) CreateUser(
+	ctx context.Context,
+	userName,
+	email,
+	password string,
+) (uuid.UUID, error) {
+
+	_, err := msu.Queries.CreateUser(ctx, CreateUserParams{
+		Name:     userName,
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	id := uuid.New()
+
+	return id, nil
+
 }
