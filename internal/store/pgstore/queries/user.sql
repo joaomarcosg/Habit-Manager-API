05a5053685b90b23347d5686970d0da6d09402d3
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-18 21:20:46 -0300

    FIX: fixes metada

--- internal/store/pgstore/queries/user.sql
+++ internal/store/pgstore/queries/user.sql
@@ -1,14 +1,14 @@
---name: CreateUser :one
+-- name: CreateUser :one
 INSERT INTO users ("name", "email", "password_hash")
 VALUES ($1, $2, $3)
 RETURNING id;
 
---name: GetUserById :one
+-- name: GetUserById :one
 SELECT id, name, password_hash, created_at, updated_at
 FROM users
 WHERE id = $1;
 
---name: GetUserByEmail :one
+-- name: GetUserByEmail :one
 SELECT id, name, password_hash, created_at, updated_at
 FROM users
 WHERE email = $1;
