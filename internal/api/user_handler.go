28a2084ac6a0c67b4ab76d554bf3045b39c04739
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-07 16:29:25 -0300

    FEAT: adds signup user handler

---
+++ internal/api/user_handler.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/joaomarcosg/Habit-Manager-API/internal/jsonutils"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/usecase/user"
+)
+
+func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
+
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.CreateUser(
+		r.Context(),
+		data.Name,
+		data.Email,
+		data.Password,
+	)
+
+}
