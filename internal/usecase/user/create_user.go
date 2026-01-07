2b0505500d9d917b385efb623df8bd79d4387810
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-07 16:01:48 -0300

    FEAT: finishes user request validation

--- internal/usecase/user/create_user.go
+++ internal/usecase/user/create_user.go
@@ -1,7 +1,24 @@
 package user
 
+import (
+	"context"
+
+	"github.com/joaomarcosg/Habit-Manager-API/internal/validator"
+)
+
 type CreateUserReq struct {
 	Name     string `json:"user_name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (req *CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
+	var eval validator.Evaluator
+
+	eval.CheckField(validator.NotBlank(req.Name), "user_name", "this field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Email), "email", "this field cannot be empty")
+	eval.CheckField(validator.Matches(req.Email, validator.EmailRX), "email", "must be a valid email")
+	eval.CheckField(validator.MinChars(req.Password, 8), "password", "password must be bigger than 8 chars")
+
+	return eval
 }
