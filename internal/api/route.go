0fd04595d56b892cf0d463cc6f7f77404a6fa985
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-07 16:20:46 -0300

    FEAT: adds initial settings to create the routes

---
+++ internal/api/route.go
@@ -0,0 +1,13 @@
+package api
+
+import "github.com/go-chi/chi/v5"
+
+func (api *Api) BindRoutes() {
+	api.Router.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			r.Route("/users", func(r chi.Router) {
+
+			})
+		})
+	})
+}
