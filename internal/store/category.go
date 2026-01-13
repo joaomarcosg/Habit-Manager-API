0768a63909591e679426a4fb644e03dbe155a958
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-13 17:06:47 -0300

    FEAT: adds category interface

--- internal/store/category.go
+++ internal/store/category.go
@@ -1,1 +1,15 @@
 package store
+
+import "context"
+
+type Category struct {
+	Name    string `json:"category_name"`
+	Entries int    `json:"entries"`
+}
+
+type CategoryStore interface {
+	CreateCategory(ctx context.Context, name string) (Category, error)
+	DeleteCategory(ctx context.Context, name string) (bool, error)
+	GetCategoryByName(ctx context.Context, name string) (Category, error)
+	GetCategoryEntries(ctx context.Context, name string) (Category, error)
+}
