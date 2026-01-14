e1d3947b2142db6992111e316a616fddbfcdf26d
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-14 16:44:21 -0300

    FEAT: adds CreatedAt and UpdatedAt fields

--- internal/store/category.go
+++ internal/store/category.go
@@ -2,14 +2,17 @@ package store
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 )
 
 type Category struct {
-	ID      uuid.UUID `json:"category_id"`
-	Name    string    `json:"category_name"`
-	Entries int       `json:"entries"`
+	ID        uuid.UUID `json:"category_id"`
+	Name      string    `json:"category_name"`
+	Entries   int       `json:"entries"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type CategoryStore interface {
