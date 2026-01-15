17e94f6f091f3e318bd70bdb5ab3920c38decfe0
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-15 15:58:20 -0300

    FIX: adds created_at and updated_at columns

--- internal/store/pgstore/queries/category.sql
+++ internal/store/pgstore/queries/category.sql
@@ -4,12 +4,12 @@ VALUES ($1)
 RETURNING id;
 
 -- name: GetCategoryById :one
-SELECT id, name, entries
+SELECT id, name, entries, created_at, updated_at
 FROM categories
 WHERE id = $1;
 
 -- name: GetCategoryByName :one
-SELECT id, name, entries
+SELECT id, name, entries, created_at, updated_at
 FROM categories
 WHERE name = $1;
 
