0b1a6efe8b969490418afadb2fc4c6136ee630e1
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-16 15:57:48 -0300

    TEST: adds initial settings for test

---
+++ internal/services/category_service_test.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/store"
+)
+
+type MockCategoryStore struct{}
+
+func (m *MockCategoryStore) CreateCategory(ctx context.Context, name string) (uuid.UUID, error) {
+	id := uuid.New()
+	return id, nil
+}
+
+func (m *MockCategoryStore) GetCategoryById(ctx context.Context, id uuid.UUID) (store.Category, error) {
+	return store.Category{}, nil
+}
+
+func (m *MockCategoryStore) GetCategoryByName(ctx context.Context, name string) (store.Category, error) {
+	return store.Category{}, nil
+}
+
+func (m *MockCategoryStore) GetCategoryEntries(ctx context.Context, name string) (store.Category, error) {
+	return store.Category{}, nil
+}
+
+func (m *MockCategoryStore) DeleteCategory(ctx context.Context, id uuid.UUID) (bool, error) {
+	return true, nil
+}
