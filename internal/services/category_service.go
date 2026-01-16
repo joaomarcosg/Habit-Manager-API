efa25cda73bc4047edafe00461afc03c57abacd4
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-16 16:20:21 -0300

    FEAT: adds create category method

--- internal/services/category_service.go
+++ internal/services/category_service.go
@@ -1,6 +1,15 @@
 package services
 
-import "github.com/joaomarcosg/Habit-Manager-API/internal/store"
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/store"
+)
+
+var ErrDuplicateCategoryName = errors.New("category name already exists")
 
 type CategoryService struct {
 	Store store.CategoryStore
@@ -10,4 +19,16 @@ func NewCategoryService(store store.CategoryStore) *CategoryService {
 	return &CategoryService{
 		Store: store,
 	}
+}
+
+func (cs *CategoryService) CreateCategory(ctx context.Context, name string) (uuid.UUID, error) {
+	id, err := cs.Store.CreateCategory(ctx, name)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return uuid.UUID{}, err
+		}
+	}
+
+	return id, nil
 }
