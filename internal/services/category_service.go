fac04f9287b054815f8128573af61d29838c6bb1
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-20 16:27:32 -0300

    FEAT: adds GetCategoryById method

--- internal/services/category_service.go
+++ internal/services/category_service.go
@@ -5,11 +5,15 @@ import (
 	"errors"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/joaomarcosg/Habit-Manager-API/internal/store"
 )
 
-var ErrDuplicateCategoryName = errors.New("category name already exists")
+var (
+	ErrDuplicateCategoryName = errors.New("category name already exists")
+	ErrCategoryNotFound      = errors.New("category not found")
+)
 
 type CategoryService struct {
 	Store store.CategoryStore
@@ -31,4 +35,22 @@ func (cs *CategoryService) CreateCategory(ctx context.Context, name string) (uui
 	}
 
 	return id, nil
+}
+
+func (cs *CategoryService) GetCategoryById(ctx context.Context, id uuid.UUID) (store.Category, error) {
+	category, err := cs.Store.GetCategoryById(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return store.Category{}, ErrCategoryNotFound
+		}
+		return store.Category{}, err
+	}
+
+	return store.Category{
+		ID:        category.ID,
+		Name:      category.Name,
+		Entries:   category.Entries,
+		CreatedAt: category.CreatedAt,
+		UpdatedAt: category.UpdatedAt,
+	}, nil
 }
