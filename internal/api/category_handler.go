36398fa38f4a56c5f8d0d4141128a477df9ec0b4
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-21 16:44:13 -0300

    FEAT: adds create category handler

--- internal/api/category_handler.go
+++ internal/api/category_handler.go
@@ -1,1 +1,36 @@
 package api
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/joaomarcosg/Habit-Manager-API/internal/jsonutils"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/services"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/usecase/category"
+)
+
+func (api *Api) handleCreateCategory(w http.ResponseWriter, r *http.Request) {
+
+	data, problems, err := jsonutils.DecodeValidJson[category.CreateCategoryReq](r)
+
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.CategoryService.CreateCategory(r.Context(), data.Name)
+
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicateCategoryName) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "email or username already exists",
+			})
+			return
+		}
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_id": id,
+	})
+
+}
