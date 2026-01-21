43457519bc92df22713e4668dbfba882aaa1c74c
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-21 16:14:08 -0300

    TEST: adds create category handler test

--- internal/api/category_handler_test.go
+++ internal/api/category_handler_test.go
@@ -1,7 +1,12 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	"github.com/google/uuid"
 	"github.com/joaomarcosg/Habit-Manager-API/internal/store"
@@ -23,4 +28,30 @@ func (m *MockCategoryStore) GetCategoryByName(ctx context.Context, name string) 
 
 func (m *MockCategoryStore) DeleteCategory(ctx context.Context, id uuid.UUID) (bool, error) {
 	return true, nil
+}
+
+func TestCreateCategory(t *testing.T) {
+	api := Api{}
+
+	payLoad := map[string]any{
+		"name": "Health",
+	}
+
+	body, err := json.Marshal(payLoad)
+	if err != nil {
+		t.Fatal("fail to parse request payload")
+	}
+
+	req := httptest.NewRequest("POST", "api/v1/categories", bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(api.handleCreateCategory)
+	handler.ServeHTTP(rec, req)
+
+	t.Logf("Rec body %s\n", rec.Body.Bytes())
+
+	if rec.Code != http.StatusCreated {
+		t.Errorf("Statuscode differs; got %d | want %d", rec.Code, http.StatusCreated)
+	}
 }
