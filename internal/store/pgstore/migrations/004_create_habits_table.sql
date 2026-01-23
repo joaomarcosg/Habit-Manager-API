dc0714e2e1d36f16daee9c3fc871cb5db2b310a6
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-23 16:29:02 -0300

    FEAT: adds create table habits migration

---
+++ internal/store/pgstore/migrations/004_create_habits_table.sql
@@ -0,0 +1,25 @@
+-- Write your migrate up statements here
+
+CREATE TYPE weekday AS ENUM (
+    'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturnday', 'sunday'
+);
+
+CREATE TABLE IF NOT EXISTS habits (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    name VARCHAR(100) UNIQUE NOT NULL,
+    category VARCHAR(100) UNIQUE NOT NULL,
+    description TEXT NOT NULL,
+    frequency weekday[] NOT NULL,
+    start_date DATE,
+    target_date DATE,
+    priority SMALLINT NOT NULL CHECK(priority BETWEEN 0 AND 1O),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS habits;
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
