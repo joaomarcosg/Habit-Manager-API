51c26332565c0adbd8e72e8eb7e8a8c614e8bfc0
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-01-23 16:36:05 -0300

    FIX: fixes syntax

--- internal/store/pgstore/migrations/004_create_habits_table.sql
+++ internal/store/pgstore/migrations/004_create_habits_table.sql
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS habits (
     frequency weekday[] NOT NULL,
     start_date DATE,
     target_date DATE,
-    priority SMALLINT NOT NULL CHECK(priority BETWEEN 0 AND 1O),
+    priority SMALLINT NOT NULL CHECK (priority BETWEEN 0 AND 10),
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 ---- create above / drop below ----
