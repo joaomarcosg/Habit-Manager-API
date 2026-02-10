2420812e2f8347e9ad67700195d6ba329772d36a
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-10 16:40:40 -0300

    FEAT: adds CreateHabit service

--- internal/services/habit_service.go
+++ internal/services/habit_service.go
@@ -1,6 +1,19 @@
 package services
 
-import "github.com/joaomarcosg/Habit-Manager-API/internal/store"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/store"
+)
+
+var (
+	ErrDuplicateHabitName = errors.New("habit name already exists")
+	ErrHabitNotFound      = errors.New("habit not found")
+)
 
 type HabitService struct {
 	Store store.HabitStore
@@ -10,4 +23,39 @@ func NewHabitService(store store.HabitStore) *HabitService {
 	return &HabitService{
 		Store: store,
 	}
+}
+
+func (hs *HabitService) CreateHabit(
+	ctx context.Context,
+	name,
+	category,
+	description string,
+	frequency []store.WeekDay,
+	startDate,
+	targetDate time.Time,
+	priority int,
+) (uuid.UUID, error) {
+
+	id, err := hs.Store.CreateHabit(
+		ctx,
+		name,
+		category,
+		description,
+		frequency,
+		startDate,
+		targetDate,
+		priority,
+	)
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return uuid.UUID{}, ErrDuplicateHabitName
+		}
+
+		return uuid.UUID{}, err
+	}
+
+	return id, nil
+
 }
