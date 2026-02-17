f95d2d5c2634a7a68ffbb209490be6af06eb5b8d
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-17 11:32:18 -0300

    FIX: fixes interface return values

--- internal/store/category.go
+++ internal/store/category.go
@@ -2,22 +2,14 @@ package store
 
 import (
 	"context"
-	"time"
 
 	"github.com/google/uuid"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/domain"
 )
-
-type Category struct {
-	ID        uuid.UUID `json:"category_id"`
-	Name      string    `json:"category_name"`
-	Entries   int       `json:"entries"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
 
 type CategoryStore interface {
 	CreateCategory(ctx context.Context, name string) (uuid.UUID, error)
-	GetCategoryById(ctx context.Context, id uuid.UUID) (Category, error)
-	GetCategoryByName(ctx context.Context, name string) (Category, error)
+	GetCategoryById(ctx context.Context, id uuid.UUID) (domain.Category, error)
+	GetCategoryByName(ctx context.Context, name string) (domain.Category, error)
 	DeleteCategory(ctx context.Context, id uuid.UUID) (bool, error)
 }
