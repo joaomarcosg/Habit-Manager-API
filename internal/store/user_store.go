91a29ceec7539e8f8de8ccae9211abfcf2597272
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-17 11:55:08 -0300

    FIX: fixes interface return values

--- internal/store/user_store.go
+++ internal/store/user_store.go
@@ -2,23 +2,14 @@ package store
 
 import (
 	"context"
-	"time"
 
 	"github.com/google/uuid"
+	"github.com/joaomarcosg/Habit-Manager-API/internal/domain"
 )
-
-type User struct {
-	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
-	Createdat time.Time `json:"created_at"`
-	Updatedat time.Time `json:"updated_at"`
-}
 
 type UserStore interface {
 	CreateUser(ctx context.Context, userName, email string, password []byte) (uuid.UUID, error)
 	AuthenticateUser(ctx context.Context, email, password string) (uuid.UUID, error)
-	GetUserByEmail(ctx context.Context, email string) (User, error)
-	GetUserById(ctx context.Context, id uuid.UUID) (User, error)
+	GetUserByEmail(ctx context.Context, email string) (domain.User, error)
+	GetUserById(ctx context.Context, id uuid.UUID) (domain.User, error)
 }
