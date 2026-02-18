80032b1fbc8df90b8ac84ab3fcdad749e71708e1
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-18 11:33:48 -0300

    FEAT: adds GetCategoryByName handler

--- internal/api/category_handler.go
+++ internal/api/category_handler.go
@@ -34,3 +34,29 @@ func (api *Api) handleCreateCategory(w http.ResponseWriter, r *http.Request) {
 	})
 
 }
+
+func (api *Api) handleGetCategoryByName(w http.ResponseWriter, r *http.Request) {
+
+	data, problems, err := jsonutils.DecodeValidJson[category.CreateCategoryReq](r)
+
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	category, err := api.CategoryService.GetCategoryByName(r.Context(), data.Name)
+
+	if err != nil {
+		if errors.Is(err, services.ErrCategoryNotFound) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusNotFound, map[string]any{
+				"error": "category not found",
+			})
+			return
+		}
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusOK, map[string]any{
+		"category": category,
+	})
+
+}
