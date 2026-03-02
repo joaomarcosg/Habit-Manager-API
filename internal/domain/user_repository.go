f9b750565e2672ce2034d17458e2935b300bacd1
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-02 12:11:53 -0300

    FIX: fixes interface parameter

--- internal/domain/user_repository.go
+++ internal/domain/user_repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type UserRepository interface {
-	CreateUser(ctx context.Context, userName, email string, password []byte) (uuid.UUID, error)
+	CreateUser(ctx context.Context, user User) (uuid.UUID, error)
 	AuthenticateUser(ctx context.Context, email, password string) (uuid.UUID, error)
 	GetUserByEmail(ctx context.Context, email string) (User, error)
 	GetUserById(ctx context.Context, id uuid.UUID) (User, error)
