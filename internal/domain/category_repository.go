c38ce10bdb474d859c33e5d4b03be52f30fc88b1
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-06 16:18:08 -0300

    REFACTOR: removes unecessary interface method

--- internal/domain/category_repository.go
+++ internal/domain/category_repository.go
@@ -9,6 +9,5 @@ import (
 type CategoryRepository interface {
 	CreateCategory(ctx context.Context, name string) (uuid.UUID, error)
 	GetCategoryByName(ctx context.Context, name string) (Category, error)
-	GetCategoryEntries(ctx context.Context, name string) (int, error)
 	DeleteCategory(ctx context.Context, name string) (bool, error)
 }
