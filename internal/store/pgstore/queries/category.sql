c92e67383e6f44456c1b4248fc785ef97ec7c13c
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-06 16:28:24 -0300

    REFACTOR: removes unecessary queries and modify DeleteCategory query variable

--- internal/store/pgstore/queries/category.sql
+++ internal/store/pgstore/queries/category.sql
@@ -3,11 +3,6 @@ INSERT INTO categories ("name")
 VALUES ($1)
 RETURNING id;
 
--- name: GetCategoryById :one
-SELECT id, name, entries, created_at, updated_at
-FROM categories
-WHERE id = $1;
-
 -- name: GetCategoryByName :one
 SELECT id, name, entries, created_at, updated_at
 FROM categories
@@ -20,4 +15,4 @@ WHERE name = $1;
 
 -- name: DeleteCategory :execresult
 DELETE FROM categories
-WHERE id = $1;
+WHERE name = $1;
