6263bff32e1a52c884b3a4746662f43b9e6a9276
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-19 16:41:05 -0300

    feat: adds IncrementEntries interface method

--- internal/domain/category_repository.go
+++ internal/domain/category_repository.go
@@ -9,5 +9,6 @@ import (
 type CategoryRepository interface {
 	CreateCategory(ctx context.Context, category Category) (uuid.UUID, error)
 	GetCategoryByName(ctx context.Context, name string) (Category, error)
+	IncrementEntries(ctx context.Context, name string) error
 	DeleteCategory(ctx context.Context, name string) error
 }
